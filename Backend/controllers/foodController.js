6c8d1ca89fbd9e697a775179e7d05795899b4a13
VARUN D <153415892+Varun31210@users.noreply.github.com>
2025-10-04 00:25:12 +0530

    7

--- Backend/controllers/foodController.js
+++ Backend/controllers/foodController.js
@@ -6,7 +6,7 @@ import fs from 'fs';
 const addFood = async(req,res) => {
 
     let image_filename = `${req.file.filename}`;
-
+  
     const food = new foodModel({
         name : req.body.name,
         description: req.body.description,
@@ -24,5 +24,34 @@ const addFood = async(req,res) => {
     }
 
 }
+//all food list
 
-export {addFood}
+const listFood = async(req,res) => {
+    try{
+        const foods = await foodModel.find({});
+        res.json({success : true , data:foods})
+    }catch(error){
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
+}
+
+//remove food item
+
+const removeFood = async(req,res) => {
+    try{
+        const food = await foodModel.findById(req.body.id)
+        fs.unlink(`uploads/${food.image}`,()=>{})
+
+        await foodModel.findByIdAndDelete(req.body.id);
+        res.json({success:true,message:"Food Removed"})
+    }
+    catch(error){
+        console.log(error);
+        res.json({success:false,message:"Error"})
+        
+    }
+}
+
+
+export {addFood,listFood,removeFood}
